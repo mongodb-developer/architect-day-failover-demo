4278437bb7a757f2fb4e574e707b909098d9b637
diana esteves <diana@atxyall.com>
2025-11-06 14:21:46 -0600

    added ports

--- scripts/init-rs.js
+++ scripts/init-rs.js
@@ -11,9 +11,9 @@ rs.initiate({
   _id: "mongodb-repl-set",
   version: 1,
   members: [
-    { _id: 0, host: "mongo0" },
-    { _id: 1, host: "mongo1" },
-    { _id: 2, host: "mongo2" }
+    { _id: 0, host: "mongo0:27017" },
+    { _id: 1, host: "mongo1:27017" },
+    { _id: 2, host: "mongo2:27017" }
   ]
 });
 print("Replica set initiated successfully");
